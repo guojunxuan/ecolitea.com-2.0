3871b279e74d9f8344c89b286de738d739711106
郭生 <jason@Guos-MacBook-Air.local>
2026-08-29 22:59:17 +0800

    feat: configure Payload cloud storage plugin

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -12,6 +12,9 @@ import { beforeSyncWithSearch } from '@/search/beforeSync'
 
 import { Page, Post } from '@/payload-types'
 import { getServerSideURL } from '@/utilities/getURL'
+import { cloudStoragePlugin } from '@payloadcms/plugin-cloud-storage'
+import { s3Storage } from '@payloadcms/storage-s3'
+import { serverEnv } from '@/utilities/env'
 
 const generateTitle: GenerateTitle<Post | Page> = ({ doc }) => {
   return doc?.title ? `${doc.title} | Payload Website Template` : 'Payload Website Template'
@@ -24,6 +27,9 @@ const generateURL: GenerateURL<Post | Page> = ({ doc }) => {
 }
 
 export const plugins: Plugin[] = [
+  cloudStoragePlugin({
+    collections: { media: { adapter: s3Storage({ collections: { media: true }, bucket: serverEnv.R2_BUCKET, config: { credentials: { accessKeyId: serverEnv.R2_ACCESS_KEY_ID, secretAccessKey: serverEnv.R2_SECRET_ACCESS_KEY }, endpoint: serverEnv.R2_ENDPOINT, forcePathStyle: true, region: 'auto' } }) } },
+  }),
   redirectsPlugin({
     collections: ['pages', 'posts'],
     overrides: {
